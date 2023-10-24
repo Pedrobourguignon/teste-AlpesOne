096bbb573f75b2f8a9d64af1661be5914cdf642f
Pedrobourguignon <pedrobourguignon@hotmail.com>
2023-10-24 17:05:40 -0300

    feat: create request cotation component

---
+++ components/RequestQuote.tsx
@@ -0,0 +1,11 @@
+import "../styles/components/requestQuote.sass";
+
+export const RequestQuote = () => {
+  return (
+    <div className="request-quote">
+      <div className="request-quote-content">
+        <a>Solicite uma cotação</a>
+      </div>
+    </div>
+  );
+};
